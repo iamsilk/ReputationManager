1b79b00298dcbb1cb9d7dd58920964a6f719702a
IAmSilK <iamsilk3495@gmail.com>
2017-06-28 22:02:39 -0300

    Created repository.

---
+++ CommandReputation.cs
@@ -0,0 +1,80 @@
+﻿using Rocket.API;
+using Rocket.Unturned.Chat;
+using Rocket.Unturned.Player;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ReputationManager
+{
+    public class CommandReputation : IRocketCommand
+    {
+        public string Name => "reputation";
+
+        public string Help => "Change someone's reputation.";
+
+        public string Syntax => "[player] <reputation>";
+
+        public AllowedCaller AllowedCaller => AllowedCaller.Both;
+
+        public List<string> Permissions => new List<string>() { "reputation" };
+
+        public List<string> Aliases => new List<string>() { "rep" };
+
+        public void Execute(IRocketPlayer player, string[] parameters)
+        {
+            if (parameters.Length == 0 || parameters.Length > 2)
+            {
+                UnturnedChat.Say(player, ReputationManager.Instance.Translate("help_reputation"));
+                return;
+            }
+
+            if (parameters.Length == 1)
+            {
+                if (player is ConsolePlayer)
+                {
+                    UnturnedChat.Say(player, ReputationManager.Instance.Translate("console_cannot_call"), UnityEngine.Color.red);
+                    return;
+                }
+
+                int reputation;
+                if (!int.TryParse(parameters[0], out reputation))
+                {
+                    UnturnedChat.Say(player, ReputationManager.Instance.Translate("invalid_reputation"), UnityEngine.Color.red);
+                    return;
+                }
+
+                ReputationManager.SetReputation((UnturnedPlayer)player, reputation);
+
+                UnturnedChat.Say(player, ReputationManager.Instance.Translate("reputation_set_success", reputation));
+            }
+            else
+            {
+                if (!player.HasPermission("reputation.other"))
+                {
+                    UnturnedChat.Say(player, ReputationManager.Instance.Translate("no_permission"), UnityEngine.Color.red);
+                    return;
+                }
+
+                UnturnedPlayer target = UnturnedPlayer.FromName(parameters[0]);
+                if (target == null)
+                {
+                    UnturnedChat.Say(player, ReputationManager.Instance.Translate("player_not_found"), UnityEngine.Color.red);
+                    return;
+                }
+                
+                int reputation;
+                if (!int.TryParse(parameters[1], out reputation))
+                {
+                    UnturnedChat.Say(player, ReputationManager.Instance.Translate("invalid_reputation"), UnityEngine.Color.red);
+                    return;
+                }
+
+                ReputationManager.SetReputation(target, reputation);
+
+                UnturnedChat.Say(player, ReputationManager.Instance.Translate("reputation_set_other_success", target.DisplayName, reputation));
+            }
+        }
+    }
+}
---
+++ ReputationManager.cs
@@ -0,0 +1,38 @@
+﻿using Rocket.Core.Plugins;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Rocket.Unturned.Player;
+using Rocket.API.Collections;
+
+namespace ReputationManager
+{
+    public class ReputationManager : RocketPlugin
+    {
+        public static ReputationManager Instance;
+
+        protected override void Load()
+        {
+            Instance = this;
+        }
+
+        public static void SetReputation(UnturnedPlayer player, int reputation)
+        {
+            // player.Reputation.set adds the value. Work around to set it specifically.
+            player.Reputation = reputation - player.Reputation;
+        }
+
+        public override TranslationList DefaultTranslations => new TranslationList()
+        {
+            { "help_reputation", "/reputation [player] <amount> - Set your or another player's reputation to the specified amount." },
+            { "reputation_set_success", "Successfully set your reputation to {0}." },
+            { "reputation_set_other_success", "Successfully set {0}'s reputation to {1}." },
+            { "player_not_found", "The specified player cannot be found." },
+            { "console_cannot_call", "The console cannot set it's own reputation." },
+            { "invalid_reputation", "The specified reputation is invalid." },
+            { "no_permission", "You do not have permission to do this." },
+            { "invalid_parameters", "Invalid parameters." },
+        };
+    }
+}
